efd9e6fda6a092f8b84d0026ccdde53da2b17089
Anton Belski <anthony.belski@gmail.com>
2019-03-16 18:19:53 +0300

    Add insertion sort

---
+++ ADS/Sort/insertion_sort.py
@@ -0,0 +1,12 @@
+# Insertion sort (Сортировка вставками)
+
+
+def insert_sort(array):
+    if len(array) < 2:
+        return array
+    for i in range(1, len(array)):
+        j = i
+        while j > 0 and array[j-1] > array[j]:
+            array[j-1], array[j] = array[j], array[j-1]
+            j = j - 1
+    return array

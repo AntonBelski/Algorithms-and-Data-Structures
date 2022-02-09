0bdf5c0228d28bc7a0308ac77fe38a9100efc931
AntonBelski <anthony.belski@gmail.com>
2022-02-09 11:30:30 +0300

    small improvement for union-find data structure + add example

--- Union-Find/1. Union-Find.py
+++ Union-Find/1. Union-Find.py
@@ -1,13 +1,13 @@
 class UnionFind:
     def __init__(self, n):
         # Time Complexity - O(n)
-        self.nodes = []
-        self.sizes = []
+        self.nodes = {}
+        self.sizes = {}
 
         for i in range(n):
-            self.nodes.append(i)
+            self.nodes[i] = i
         for i in range(n):
-            self.sizes.append(1)
+            self.sizes[i] = 1
 
     def union(self, p, q):
         # Time Complexity - O(log*(n)), * -> this function veryyy close to constant O(1)
@@ -33,4 +33,11 @@ class UnionFind:
 
 
 if __name__ == '__main__':
-    pass
+    union_find = UnionFind(6)
+    union_find.union(1, 5)
+    union_find.union(0, 5)
+    union_find.union(4, 5)
+    union_find.union(4, 4)
+    union_find.union(4, 5)
+    print(union_find.nodes)
+    print(union_find.sizes)

53ec8db5af823e14af5e00c4ab413286d7cbe2d7
AntonBelski <anthony.belski@gmail.com>
2022-07-05 18:10:33 +0300

    refactor union_find.py

--- Data Structures/union_find.py
+++ Data Structures/union_find.py
@@ -1,34 +1,38 @@
 class UnionFind:
     def __init__(self, n):
         # Time Complexity - O(n)
-        self.nodes = {}
-        self.sizes = {}
+        self.nodes = []
+        self.sizes = []
 
         for i in range(n):
-            self.nodes[i] = i
-        for i in range(n):
-            self.sizes[i] = 1
+            self.nodes.append(i)
+            self.sizes.append(1)
 
     def union(self, p, q):
-        # Time Complexity - O(log*(n)), * -> this function veryyy close to constant O(1)
-        p_node_root = self.root(p)
-        q_node_root = self.root(q)
-        if self.sizes[q_node_root] > self.sizes[p_node_root]:
-            self.nodes[p_node_root] = q_node_root
-            self.sizes[q_node_root] += self.sizes[p_node_root]
+        # Time Complexity - O(log*(n)), * -> iterated logarithm, veryyy close to constant O(1)
+        p_root = self.root(p)
+        q_root = self.root(q)
+
+        if p_root == q_root:
+            return
+
+        if self.sizes[q_root] > self.sizes[p_root]:
+            self.nodes[p_root] = q_root
+            self.sizes[q_root] += self.sizes[p_root]
         else:
-            self.nodes[q_node_root] = p_node_root
-            self.sizes[p_node_root] += self.sizes[q_node_root]
+            self.nodes[q_root] = p_root
+            self.sizes[p_root] += self.sizes[q_root]
 
     def root(self, e):
-        # Time Complexity - O(log*(n)), * -> this function veryyy close to constant O(1)
+        # Time Complexity - O(log*(n)), * -> iterated logarithm, veryyy close to constant O(1)
+        # Tarjan and Van Leeuwen one-pass improvement, Here is - Path splitting
+        # Good description - https://en.wikipedia.org/wiki/Disjoint-set_data_structure
         while e != self.nodes[e]:
-            self.nodes[e] = self.nodes[self.nodes[e]]
-            e = self.nodes[e]
+            e, self.nodes[e] = self.nodes[e], self.nodes[self.nodes[e]]
         return e
 
     def connected(self, p, q):
-        # Time Complexity - O(log*(n)), * -> this function veryyy close to constant O(1)
+        # Time Complexity - O(log*(n)), * -> iterated logarithm, veryyy close to constant O(1)
         return self.root(p) == self.root(q)
 
 
